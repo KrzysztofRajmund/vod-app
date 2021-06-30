7a138fd45071afe65d393ad507220aebb27dee1d
Krzysztof Nadlonek <knadlonek@gmail.com>
2021-06-30 10:19:29 +0200

    added theme context template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import { ThemeContext } from '@emotion/react';
+import React, { useContext, useEffect } from 'react';
 //components
 import SplashScreen from './components/SplashScreen/SplashScreen';
+//context
+import { ContextUserLogin } from './components/utils/ThemeContext';
+
 
 const App: React.FC = () => {
+
+  const loginUser = () => {
+    console.log('i am login');
+  }
+
   return (
-    <>
-      <SplashScreen />
-    </>
+    <ThemeContext.Provider value={{}}>
+      <SplashScreen loginUser={loginUser} />
+    </ThemeContext.Provider>
   );
 };
 
